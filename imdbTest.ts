02d2fa76a6dc9f41735f9936f9c8b282ce09c490
Zetsuga <darkbear24@gmail.com>
2022-01-24 12:37:08 +0100

    actualizaciones varias

---
+++ imdbTest.ts
@@ -0,0 +1,8 @@
+import {Professionals} from "./professionals";
+import {Movie} from "./movie";
+import {Imdb} from "./imdb";
+
+let actor1:Professionals = new Professionals();
+let pelicula:Movie = new Movie();
+let imdb:Imdb = new Imdb();
+
